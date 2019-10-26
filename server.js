a57cf01c631e401f166be3db56f038360b7812c3
Grigoriy Levanov <glevanov@gmail.com>
2019-10-26 10:00:41 +0300

    add server stub

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 1066;
+
+app.listen(port, () => {
+   console.log(`Server is listening on port ${port}. Ready to accept requests!`)
+});
