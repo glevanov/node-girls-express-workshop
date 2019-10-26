e326904bd97ff243defd76c39d0417cb5c904e65
Grigoriy Levanov <glevanov@gmail.com>
2019-10-26 18:35:51 +0300

    add /get-posts endpoint

--- server.js
+++ server.js
@@ -11,17 +11,23 @@ app.listen(port, () => {
    console.log(`Server is listening on port ${port}. Ready to accept requests!`)
 });
 
+const postsPath = __dirname + '/data/posts.json';
+
 app.post('/create-post', (req, res) => {
    const postsPath = __dirname + '/data/posts.json';
    fs.readFile(postsPath, 'utf-8', (err, file) => {
       if (err) { console.log(err) }
       const posts = JSON.parse(file);
-      posts[new Date().valueOf()] = req.fields.blogpost;
+      posts[Date.now()] = req.fields.blogpost;
 
       fs.writeFile(postsPath, JSON.stringify(posts), 'utf-8', (err) => {
          if (err) { console.log(err) }
       })
    });
+});
+
+app.get('/get-posts', (req, res) => {
+    res.sendFile(postsPath);
 });
 
 app.use(express.static('public'));
