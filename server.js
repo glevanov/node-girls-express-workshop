d9a806c2af1c77ee8c8a1dbfad3ce62f83254284
Grigoriy Levanov <glevanov@gmail.com>
2019-10-26 10:04:37 +0300

    add route handler stub

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 1066;
 app.listen(port, () => {
    console.log(`Server is listening on port ${port}. Ready to accept requests!`)
 });
+
+app.get('/', (req, res) => {
+   res.send('Request recieved.')
+});
