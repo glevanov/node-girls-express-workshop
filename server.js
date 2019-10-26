6e02720e81875725414eef7ac0a50e2e794df96b
Grigoriy Levanov <glevanov@gmail.com>
2019-10-26 16:56:58 +0300

    add post add feature

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const formidable = require('express-formidable');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 1066;
@@ -11,7 +12,16 @@ app.listen(port, () => {
 });
 
 app.post('/create-post', (req, res) => {
-   console.log(req.fields);
+   const postsPath = __dirname + '/data/posts.json';
+   fs.readFile(postsPath, 'utf-8', (err, file) => {
+      if (err) { console.log(err) }
+      const posts = JSON.parse(file);
+      posts[new Date().valueOf()] = req.fields.blogpost;
+
+      fs.writeFile(postsPath, JSON.stringify(posts), 'utf-8', (err) => {
+         if (err) { console.log(err) }
+      })
+   });
 });
 
 app.use(express.static('public'));
